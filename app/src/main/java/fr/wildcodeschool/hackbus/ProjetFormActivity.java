821c4fc6617cd46a36646548a4ccbd59a726bfe1
Trempette <edouard.kerhir@gmail.com>
2019-01-16 13:47:35 +0100

    debut du formulaire de projet

---
+++ app/src/main/java/fr/wildcodeschool/hackbus/ProjetFormActivity.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.hackbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjetFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projet_form);
+    }
+}
