b1fd7e21c824f741e8e7c5543fee3382d31fce81
Romain Marca <rmarca@hotmail.fr>
2019-01-17 11:07:19 +0100

    fix

--- app/src/main/java/fr/wildcodeschool/hackbus/UserPage.java
+++ app/src/main/java/fr/wildcodeschool/hackbus/UserPage.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.wildcodeschool.hackbus.models.CompetenceModel;
+import fr.wildcodeschool.hackbus.models.ProjetModel;
 import fr.wildcodeschool.hackbus.models.UserModel;
 
 public class UserPage extends AppCompatActivity {
@@ -32,8 +33,8 @@ public class UserPage extends AppCompatActivity {
         LinearLayoutManager LayoutManager2 = new LinearLayoutManager(this);
         projetTeamList.setLayoutManager(LayoutManager2);
 
-        List<UserModel> userTeam = new ArrayList<>();
-        ProjetTeamAdapter adapter2 = new ProjetTeamAdapter(userTeam, this);
+        List<ProjetModel> projetTeam = new ArrayList<>();
+        ProjetTeamAdapter adapter2 = new ProjetTeamAdapter(projetTeam, this);
         competenceList.setAdapter(adapter2);
 
         //projet init
@@ -41,8 +42,8 @@ public class UserPage extends AppCompatActivity {
         LinearLayoutManager LayoutManager3 = new LinearLayoutManager(this);
         projetInitList.setLayoutManager(LayoutManager3);
 
-        List<UserModel> userInit = new ArrayList<>();
-        ProjetInitAdapter adapter3 = new ProjetInitAdapter(userInit, this);
+        List<ProjetModel> projetInit = new ArrayList<>();
+        ProjetInitAdapter adapter3 = new ProjetInitAdapter(projetInit, this);
         competenceList.setAdapter(adapter3);
 
     }
