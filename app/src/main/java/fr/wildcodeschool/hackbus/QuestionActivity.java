423153d2089f6eb359d846c6a299e254abacb78c
Mathilde Loiselet <mathilde.loiselet@gmail.com>
2019-01-17 16:30:45 +0100

    correction bug

--- app/src/main/java/fr/wildcodeschool/hackbus/QuestionActivity.java
+++ app/src/main/java/fr/wildcodeschool/hackbus/QuestionActivity.java
@@ -4,17 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
+import android.widget.Toast;
 
 import fr.wildcodeschool.hackbus.models.QuestionModel;
+import fr.wildcodeschool.hackbus.models.ReponseModel;
 
 public class QuestionActivity extends AppCompatActivity {
 
     private Singleton mSingleton;
+    private QuestionModel mCurrentQuestion;
+    private Boolean isCreator = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,98 +24,101 @@ public class QuestionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_question);
 
         mSingleton = Singleton.getInstance();
+        //mCurrentQuestion = mSingleton.getCurrentQuestion();
+
+        /*TODO: à décommenter quand BDD sera OK
+        if(mCurrentQuestion.getSender().getuId() == mSingleton.getUser().getuId()) {
+            isCreator = true;
+        }*/
 
         //TODO: appeler setDataQuestion & setButtons quand BDD recup'
 
-        infoButton();
+        //TODO: créer Adapter et l'appeler avec ITEM_QUESTION
     }
 
-    private void infoButton() {
-        ImageButton info = findViewById(R.id.ib_info);
-        final View greyView = findViewById(R.id.view_grey);
-        final TextView priorityMeaning = findViewById(R.id.tv_meaning_priority);
 
-        info.setOnClickListener(new View.OnClickListener() {
+    private void setButtons() {
+        Button answerButton = findViewById(R.id.send);
+        TextView openOrClose = findViewById(R.id.tv_open_close);
+        TextView closeOpenConfirmation = findViewById(R.id.tv_close_open_confirmation);
+
+        final EditText answer = findViewById(R.id.et_answer);
+
+        answerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showInfoPopup();
+                String answerText = answer.getText().toString();
+
+                if(answerText.isEmpty()){
+                    Toast.makeText(QuestionActivity.this, "Please write your answer", Toast.LENGTH_SHORT).show();
+                } else {
+                    ReponseModel newReponse = new ReponseModel(answerText, mSingleton.getUser());
+                    //TODO: à balancer vers la BDD
+                }
             }
         });
 
-        greyView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                closeInfoPopup();
-            }
-        });
+        if(isCreator && mCurrentQuestion.isOpen()) {
+            openOrClose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showInfoPopup();
+                }
+            });
 
-        priorityMeaning.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                closeInfoPopup();
+            closeOpenConfirmation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mCurrentQuestion.setOpen(false);
+                    closeInfoPopup();
+                }
+            });
 
-            }
-        });
+        }
     }
 
     private void closeInfoPopup() {
         View greyView = findViewById(R.id.view_grey);
-        TextView priorityPopup = findViewById(R.id.tv_priority_popup);
-        TextView priorityMeaning = findViewById(R.id.tv_meaning_priority);
+        TextView closeOpenPopup = findViewById(R.id.tv_close_open_popup);
+        TextView closeOpenConfirmation = findViewById(R.id.tv_close_open_confirmation);
 
         greyView.setVisibility(View.GONE);
-        priorityPopup.setVisibility(View.GONE);
-        priorityMeaning.setVisibility(View.GONE);
+        closeOpenPopup.setVisibility(View.GONE);
+        closeOpenConfirmation.setVisibility(View.GONE);
     }
 
     private void showInfoPopup() {
         View greyView = findViewById(R.id.view_grey);
-        TextView priorityPopup = findViewById(R.id.tv_priority_popup);
-        TextView priorityMeaning = findViewById(R.id.tv_meaning_priority);
+        TextView closeOpenPopup = findViewById(R.id.tv_close_open_popup);
+        TextView closeOpenConfirmation = findViewById(R.id.tv_close_open_confirmation);
 
         greyView.setVisibility(View.VISIBLE);
-        priorityPopup.setVisibility(View.VISIBLE);
-        priorityMeaning.setVisibility(View.VISIBLE);
+        closeOpenPopup.setVisibility(View.VISIBLE);
+        closeOpenConfirmation.setVisibility(View.VISIBLE);
     }
 
-    private void setButtons() {
-        Button answer = findViewById(R.id.b_i_ansawer);
-        Button noTime = findViewById(R.id.b_no_time);
-        final QuestionModel currentQuestion = mSingleton.getCurrentQuestion();
+    private void getDataQuestion() {
 
-        answer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO: à balancer vers page de réponse
-            }
-        });
-
-        noTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ArrayList<QuestionModel> questionList = mSingleton.getUser().getQuestionNeedAnswer();
-                for(int i = 0; i < questionList.size(); i ++) {
-                    if(questionList.get(i).getId() == currentQuestion.getId()) {
-                        questionList.remove(i);
-                    }
-                }
-                mSingleton.getUser().setQuestionNeedAnswer(questionList);
-                //TODO: à vérifier OU tester celle commentee
-               // mSingleton.getUser().getQuestionNeedAnswer().remove(currentQuestion);
-            }
-        });
-    }
-
-    private void setDataQuestion() {
         TextView title = findViewById(R.id.tv_question_title);
         TextView question = findViewById(R.id.tv_question);
+        TextView userNameQuestion = findViewById(R.id.tv_user_question);
+        TextView openOrClose = findViewById(R.id.tv_open_close);
 
-        QuestionModel currentQuestion = mSingleton.getCurrentQuestion();
+        String nameAndFirstLastnameLetter = mCurrentQuestion.getSender().getPrenom() + mCurrentQuestion.getSender().getNom().charAt(0);
 
-        title.setText(currentQuestion.getTitle());
-        question.setText(currentQuestion.getQuestion());
+        title.setText(mCurrentQuestion.getTitle());
+        question.setText(mCurrentQuestion.getQuestion());
+        userNameQuestion.setText(nameAndFirstLastnameLetter);
 
-        colorPriority(currentQuestion.getPriority());
+        if(!mCurrentQuestion.isOpen()) {
+            openOrClose.setText("CLOSE");
+            openOrClose.setTextColor(getResources().getColor(R.color.red));
+        } else {
+            openOrClose.setText("OPEN");
+            openOrClose.setTextColor(getResources().getColor(R.color.green));
+        }
+
+        colorPriority(mCurrentQuestion.getPriority());
     }
 
     private void colorPriority(int seekBarProgress) {
