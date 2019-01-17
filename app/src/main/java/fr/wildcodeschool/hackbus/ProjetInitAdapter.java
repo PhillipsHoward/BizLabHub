fe0f9afe01e8c5912a7f3702aac629d43814f2ed
Romain Marca <rmarca@hotmail.fr>
2019-01-17 14:54:34 +0100

    filter UserPage with Singleton

--- app/src/main/java/fr/wildcodeschool/hackbus/ProjetInitAdapter.java
+++ app/src/main/java/fr/wildcodeschool/hackbus/ProjetInitAdapter.java
@@ -46,11 +46,7 @@ public class ProjetInitAdapter extends RecyclerView.Adapter<ProjetInitAdapter.My
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
         final ProjetModel projetInit = projetInitList.get(position);
-        for (UserModel userInit: projetInit.getTeam()) {
-            if (userInit == Singleton.getInstance().getUser()) {
-                holder.nom.setText(projetInit.getNom());
-            }
-        }
+        holder.nom.setText(projetInit.getNom());
     }
 
     @Override
